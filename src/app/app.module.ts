3311287ce467568afe4521453423dcca2e99591e
rahulg3520 <rahul@nestgroup.net>
2022-11-24 12:35:59 +0530

    route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PatientEntryComponent } from './patient-entry/patient-entry.component';
 import { PatientSearchComponent } from './patient-search/patient-search.component';
 import { PatientdelComponent } from './patientdel/patientdel.component';
+
+const myRoute:Routes=[
+
+  {
+    path:"",
+    component:PatientEntryComponent
+  },
+  {
+    path:"search",
+    component:PatientSearchComponent
+  },
+  {
+    path:"delete",
+    component:PatientdelComponent
+  }
+]
+
 
 @NgModule({
   declarations: [
@@ -16,7 +34,8 @@ import { PatientdelComponent } from './patientdel/patientdel.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
